e1f7057ea9e84637e7bc8c4203a023b9d70c393e
Daniel <6657336@clases.edu.sv>
2024-07-30 22:45:13 -0600

    Agregar archivos de proyecto.

---
+++ AREAAA6657336/ViewModels/MainViewModels.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AREAAA6657336.ViewModels
+{
+    public partial class MainViewModels : ObservableObject
+    {
+        [ObservableProperty]
+        public double lado1;
+
+        [ObservableProperty]
+        public double lado2;
+
+        [ObservableProperty]
+        public double lado3;
+
+        [ObservableProperty]
+        public double superficie;
+
+        [ObservableProperty]
+        public double resultado;
+
+
+        [RelayCommand]
+        public void Calcular()
+        {
+            double Superficie = (Lado1 + Lado2 + Lado3) / 2;
+
+            double area = (Superficie * (Superficie - Lado1) * (Superficie - Lado2) * (Superficie - Lado3));
+
+            double area2 = Math.Sqrt(area);
+
+            Resultado = area2;
+        }
+    }
+}
